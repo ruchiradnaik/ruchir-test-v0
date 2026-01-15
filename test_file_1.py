a5f85cc9dc5ef34f60aef1780e2974d50453277b
RUCHIR ADNAIK <163317944+ruchiradnaik@users.noreply.github.com>
2026-01-15 14:25:31 +0530

    Uncomment langchain imports in test_file_1.py

--- test_file_1.py
+++ test_file_1.py
@@ -2,6 +2,6 @@ import pandas as pd
 import numpy as np
 import torch
 import cv2
-# from langchain_openai import ChatOpenAI
-# from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
 print("Yedya!")
